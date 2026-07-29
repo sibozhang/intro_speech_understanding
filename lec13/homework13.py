5d02a4d08bb425f9f6e75dd223a33b591f571105
Sibo Zhang <arikawaharuka@gmail.com>
2026-07-30 04:31:49 +0900

    hw13

--- lec13/homework13.py
+++ lec13/homework13.py
@@ -16,7 +16,15 @@ def lpc(speech, frame_length, frame_skip, order):
     excitation (nframes,frame_length) - linear prediction excitation frames
       (only the last frame_skip samples in each frame need to be valid)
     '''
-    raise RuntimeError("You need to write this part!")
+    # raise RuntimeError("You need to write this part!")
+    nframes = int((len(speech) - frame_length) / frame_skip)
+    frames = np.array([ speech[m*frame_skip : m*frame_skip + frame_length]
+                        for m in range(nframes) ])
+    A = librosa.lpc(frames, order=order, axis=-1)
+    excitation = np.zeros((nframes, frame_length))
+    for k in range(order + 1):
+        excitation[:, order:] += A[:, k:k+1] * frames[:, order-k : frame_length-k]
+    return A, excitation
 
 def synthesize(e, A, frame_skip):
     '''
@@ -30,7 +38,15 @@ def synthesize(e, A, frame_skip):
     @returns:
     synthesis (duration) - synthetic speech waveform
     '''
-    raise RuntimeError("You need to write this part!")
+    # raise RuntimeError("You need to write this part!")
+    nframes, order = A.shape
+    synthesis = np.zeros(len(e))
+    for i in range(len(synthesis)):
+        synthesis[i] = e[i]
+        frame = int(i / frame_skip)
+        for k in range(1,order):                     
+            synthesis[i] -= A[frame, k] * synthesis[i-k]
+    return synthesis
 
 def robot_voice(excitation, T0, frame_skip):
     '''
@@ -45,5 +61,12 @@ def robot_voice(excitation, T0, frame_skip):
     gain (nframes) - gain for each frame
     e_robot (nframes*frame_skip) - excitation for the robot voice
     '''
-    raise RuntimeError("You need to write this part!")
+    #raise RuntimeError("You need to write this part!")
+    gain = np.sqrt(np.sum(np.square(excitation), axis=1))
+    nframes = len(gain)
+    e_robot = np.zeros(nframes*frame_skip)
+    e_robot[::T0] = 1
+    for i in range(len(e_robot)):
+        e_robot[i] = e_robot[i] * gain[int(i/frame_skip)]
+    return gain, e_robot
 
