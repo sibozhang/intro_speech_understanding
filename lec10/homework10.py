0119f90eeb68d25d662705541fe2138b09227847
Sibo Zhang <arikawaharuka@gmail.com>
2026-07-30 20:18:13 +0900

    hw10

--- lec10/homework10.py
+++ lec10/homework10.py
@@ -17,7 +17,32 @@ def get_features(waveform, Fs):
         Then give every non-silent segment a different label.  Repeat each label five times.
     
     '''
-    raise RuntimeError("You need to change this part")
+    # raise RuntimeError("You need to change this part")
+    VAD_windowlen = int(0.025*Fs)
+    VAD_windowskip = int(0.01*Fs)
+    VAD_frames = np.array([waveform[m:m+VAD_windowlen]
+                            for m in range(0, len(waveform)-VAD_windowlen, VAD_windowskip)])
+    L = int(0.004*Fs)
+    S = int(0.002*Fs)
+    x_frames = np.array([waveform[m+1:m+1+L]-waveform[m:m+L]
+                            for m in range(0, len(waveform)-L, S)])
+
+    energy = np.sum(np.square(VAD_frames), 1)
+    threshold = 0.1*np.amax(energy)
+    VAD = (energy > threshold).astype(int)
+
+    padded = np.concatenate([[0], VAD, [0]])          
+    diff = np.diff(padded)
+    startframes = np.flatnonzero(diff == 1)
+    endframes = np.flatnonzero(diff == -1)
+
+    labels = np.zeros(x_frames.shape[0], dtype=int)   
+    for k in range(len(startframes)):
+        labels[5*startframes[k] : 5*endframes[k]] = k+1   
+
+    mstft = np.abs(np.fft.fft(x_frames, axis=1))
+    features = 20*np.log10(np.maximum(0.001*np.amax(mstft), mstft))[:, 0:int(L/2)]
+    return features, labels
 
 def train_neuralnet(features, labels, iterations):
     '''
@@ -39,7 +64,26 @@ def train_neuralnet(features, labels, iterations):
 
     The lossvalues should be computed using a CrossEntropy loss.
     '''
-    raise RuntimeError("You need to change this part")
+    # raise RuntimeError("You need to change this part")
+    NFRAMES, NFEATS = features.shape
+    NCLASSES = int(np.amax(labels)) + 1
+    model = torch.nn.Sequential(
+        torch.nn.LayerNorm(NFEATS, dtype=float),
+        torch.nn.Linear(NFEATS, NCLASSES, dtype=float))
+    lossfunction = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters())
+
+    X = torch.tensor(features, dtype=float)
+    Y = torch.tensor(labels, dtype=int)
+    lossvalues = np.zeros(iterations)
+    for t in range(iterations):
+        z = model(X)
+        loss = lossfunction(z, Y)
+        lossvalues[t] = loss.item()
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    return model, lossvalues
 
 def test_neuralnet(model, features):
     '''
@@ -49,5 +93,7 @@ def test_neuralnet(model, features):
     @return:
     probabilities (NFRAMES, NLABELS) - model output, transformed by softmax, detach().numpy().
     '''
-    raise RuntimeError("You need to change this part")
+    # raise RuntimeError("You need to change this part")
+    testresults = model(torch.tensor(features, dtype=float))
+    return testresults.softmax(-1, dtype=float).detach().numpy()
 
